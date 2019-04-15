d357c2629d17c61f225acc36e543dc1de07dd213
Allison Snipes <ajsnipes@usfca.edu>
2019-04-15 15:41:08 -0700

    working on code

---
+++ assests/js/app.js
@@ -0,0 +1,13 @@
+d
+
+//initialize firebase
+
+  var config = {
+    apiKey: "AIzaSyBv4N-6T0mTcj-3w3gFbe89ULnsjI6be3Y",
+    authDomain: "trainscheducb.firebaseapp.com",
+    databaseURL: "https://trainscheducb.firebaseio.com",
+    projectId: "trainscheducb",
+    storageBucket: "trainscheducb.appspot.com",
+    messagingSenderId: "260908491041"
+  };
+firebase.initializeApp(config);
