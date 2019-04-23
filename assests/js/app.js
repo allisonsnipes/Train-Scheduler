25a1ae81acf5484c781ff4f7c5cd6350400474bc
Allison Snipes <ajsnipes@usfca.edu>
2019-04-23 13:46:00 -0700

    working on js files

--- assests/js/app.js
+++ assests/js/app.js
@@ -1,6 +1,4 @@
-d
-
-//initialize firebase
+// 1. initialize firebase
 
   var config = {
     apiKey: "AIzaSyBv4N-6T0mTcj-3w3gFbe89ULnsjI6be3Y",
@@ -14,6 +12,14 @@ firebase.initializeApp(config);
 
 //establish shortcut to write code
 var database = firebase.database();
+
+//2. place an event listener on the button to add more trains
+document.querySelector("#trainBut").addEventListener("click", function(event) {
+  event.preventDefault();
+})
+
+//2. gather user's input and initilize them into variables
+var tName = document.querySelector
 
 
 
