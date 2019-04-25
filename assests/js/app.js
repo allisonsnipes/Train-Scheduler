b9d1c4504f5b5ca3d927b86f4690e5fc2282db35
Allison Snipes <ajsnipes@usfca.edu>
2019-04-24 18:05:12 -0700

    saving js changes

--- assests/js/app.js
+++ assests/js/app.js
@@ -18,8 +18,25 @@ document.querySelector("#trainBut").addEventListener("click", function(event) {
   event.preventDefault();
 })
 
-//2. gather user's input and initilize them into variables
-var tName = document.querySelector
+//gather user's input and initilize them into variables
+var tName = document.querySelector("#trainName").value.trim();
+var tTime = document.querySelector("#destinationTrain").value.trim();
+var tFirstTrain = document.querySelector("#enterFT").value.trim();
+var tFreq = document.querySelector("#freq").value.trim();
+
+//create local temporary object to hold train data
+var trainData = {
+  name: tName,
+  trainTime: tTime,
+  firstTrain: tFirstTrain,
+  frequendy: tFreq
+};
+
+//upload train data to the database
+database.ref().push(trainData);
+
+//log input to the console from the train data
+console.log(trainData.name);
 
 
 
